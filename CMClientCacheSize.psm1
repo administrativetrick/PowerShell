1dc2e76ff3d02f33936620bf6d7355ceb076cfa4
administrativetrick <48927121+administrativetrick@users.noreply.github.com>
2019-03-25 12:32:07 -0700

    CMClientCacheSize
    Adds CMClientCacheSize module to PowerShell

---
+++ CMClientCacheSize.psm1
@@ -0,0 +1,66 @@
+﻿Function Get-CMClientCacheSize {
+    <#
+    .SYNOPSIS
+	    This script checks the SCCM cache size of a given computer.
+    .DESCRIPTION
+	    This script checks the SCCM cache size of a given computer.
+    .PARAMETER ComputerName
+	    The name of the computer to run this against.
+    .EXAMPLE
+        Get-CMClientCacheSize
+    .EXAMPLE
+    Get-CMClientCacheSize -ComputerName "Computer01"
+    #>
+    [CmdletBinding()]
+    Param (
+	    [Parameter(Mandatory=$false)]
+	    [string]$ComputerName = $env:COMPUTERNAME
+    )
+	
+	process
+	{
+		foreach ($computer in $ComputerName)
+		{
+			$Cache = Get-WmiObject -ComputerName $computer -namespace root\ccm\SoftMgmtAgent -class CacheConfig
+			$outputObject = [pscustomobject]@{
+				ComputerName = $Cache.PSComputerName
+				CacheSize = $Cache.size
+			}
+			$outputObject
+		}
+	}
+}
+
+Function Set-CMClientCacheSize
+{
+    <#
+    .SYNOPSIS
+	    This script checks the SCCM cache size of a given computer.
+    .DESCRIPTION
+	    This script checks the SCCM cache size of a given computer.
+    .PARAMETER ComputerName
+	    The name of the computer to run this against.
+    .EXAMPLE
+        Get-CMClientCacheSize
+    .EXAMPLE
+    Get-CMClientCacheSize -ComputerName "Computer01"
+    #>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory = $false)]
+		[string]$ComputerName = $env:COMPUTERNAME,
+		[Parameter(Mandatory = $false)]
+		[string]$CacheSize = 10240
+	)
+	
+	process
+	{
+		foreach ($computer in $ComputerName)
+		{
+			$Cache = Get-WmiObject -ComputerName $computer -namespace root\ccm\SoftMgmtAgent -class CacheConfig
+			$Cache.size = $CacheSize
+			$Cache.Put()
+			Restart-Service ccmexec
+		}
+	}
+}
